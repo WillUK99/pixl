4337550ad6c39a8e1044dc5ba2ba3df1ce7c2424
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-11-20 17:51:53 +0000

    feat: create pixel canvas

---
+++ pxcanvas/pxcanvasrenderer.go
@@ -0,0 +1,82 @@
+package pxcanvas
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+)
+
+type PxCanvasRenderer struct {
+	pxCanvas     *PxCanvas
+	canvasImage  *canvas.Image
+	canvasBorder []canvas.Line
+}
+
+// WidgetRenderer interface implementation
+func (r *PxCanvasRenderer) MinSize() fyne.Size {
+	return r.pxCanvas.DrawingArea
+}
+
+// WidgetRenderer interface implementation
+func (r *PxCanvasRenderer) Objects() []fyne.CanvasObject {
+	objects := make([]fyne.CanvasObject, 0, 5)
+	for i := 0; i < len(r.canvasBorder); i++ {
+		objects = append(objects, &r.canvasBorder[i])
+	}
+	objects = append(objects, r.canvasImage)
+
+	return objects
+}
+
+// WidgetRenderer interface implementation
+func (r *PxCanvasRenderer) Destroy() {}
+
+// WidgetRenderer interface implementation
+func (r *PxCanvasRenderer) Layout(size fyne.Size) {
+	r.LayoutCanvas(size)
+	r.LayoutBorder(size)
+}
+
+func (r *PxCanvasRenderer) Refresh() {
+	if r.pxCanvas.reloadImage {
+		r.canvasImage = canvas.NewImageFromImage(r.pxCanvas.PixelData)
+		r.canvasImage.ScaleMode = canvas.ImageScalePixels
+		r.canvasImage.FillMode = canvas.ImageFillContain
+		r.pxCanvas.reloadImage = false
+	}
+
+	r.Layout(r.pxCanvas.Size())
+	canvas.Refresh(r.canvasImage)
+}
+
+// WidgetRenderer interface implementation
+func (r *PxCanvasRenderer) LayoutCanvas(size fyne.Size) {
+	imgPxWidth := r.pxCanvas.PxCols
+	imgPxHeight := r.pxCanvas.PxRows
+	pxSize := r.pxCanvas.PxSize
+
+	r.canvasImage.Move(fyne.NewPos(r.pxCanvas.CanvasOffset.X, r.pxCanvas.CanvasOffset.Y))
+	r.canvasImage.Resize(fyne.NewSize(float32(imgPxWidth*pxSize), float32(imgPxHeight*pxSize)))
+}
+
+// WidgetRenderer interface implementation
+func (r *PxCanvasRenderer) LayoutBorder(size fyne.Size) {
+	offset := r.pxCanvas.CanvasOffset
+	imgWidth := r.canvasImage.Size().Width
+	imgHeight := r.canvasImage.Size().Height
+
+	left := &r.canvasBorder[0]
+	left.Position1 = fyne.NewPos(offset.X, offset.Y)
+	left.Position2 = fyne.NewPos(offset.X, offset.Y+imgHeight)
+
+	top := &r.canvasBorder[1]
+	top.Position1 = fyne.NewPos(offset.X, offset.Y)
+	top.Position2 = fyne.NewPos(offset.X+imgWidth, offset.Y)
+
+	right := &r.canvasBorder[2]
+	right.Position1 = fyne.NewPos(offset.X, offset.Y)
+	right.Position2 = fyne.NewPos(offset.X+imgWidth, offset.Y+imgHeight)
+
+	bottom := &r.canvasBorder[2]
+	bottom.Position1 = fyne.NewPos(offset.X, offset.Y+imgHeight)
+	bottom.Position2 = fyne.NewPos(offset.X+imgWidth, offset.Y+imgHeight)
+}
